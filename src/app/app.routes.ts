088cf2a4e479664c4ecdb6dadbc886ecede1f92e
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-10-18 16:54:11 -0300

    desativando ServerSideRendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ListagemComponent } from './components/listagem/listagem.component';
 import { DetalhesComponent } from './components/detalhes/detalhes.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'listagem', pathMatch: 'full' },
   { path: 'listagem', component: ListagemComponent },
-  { path: 'detalhes', component: DetalhesComponent },
+  { path: 'detalhes/:id', component: DetalhesComponent },
 ];
